8944e3e2f792ce4260cfbabc04f110da098b0d0b
Ignacio Vargas <ignacii31@gmail.com>
2024-11-05 18:47:15 -0600

    Creacion del proyecto Springboot en Netbeans

---
+++ src/main/java/com/empleate/EmpleateApplication.java
@@ -0,0 +1,13 @@
+package com.empleate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmpleateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmpleateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/empleate/EmpleateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.empleate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmpleateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
